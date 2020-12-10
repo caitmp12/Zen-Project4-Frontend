7cd1ddc774713d8553048dc41b3b4b8c93888f21
Caitlin Moore-Penaskovic <cait.moorepenaskovic@gmail.com>
2020-12-09 20:29:12 -0500

    fetched data and made shell of Home component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import React from "react";
 import logo from './logo.svg';
 import './App.css';
+import Home from "./components/Home"
 
 function App() {
+  const baseURL = "http://localhost:3000";
+
+  const [posts, setPosts] = React.useState([])
+
+  const getPosts = () => {
+    fetch(`${baseURL}/posts`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data)
+        setPosts(data)
+      })
+  }
+
+
+  React.useEffect(() => {
+    getPosts()
+  }, [])
+
   return (
+    <>
     <h1>Hi</h1>
+    <Home />
+    </>
   );
 }
 
