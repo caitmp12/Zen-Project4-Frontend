74ff2cefb3ef7a3df569c9ad279f02d48cbbac65
Caitlin Moore-Penaskovic <cait.moorepenaskovic@gmail.com>
2020-12-12 12:55:26 -0500

    updated baseURL

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import New from "./components/New"
 import Show from "./components/Show"
 
 function App() {
-  const baseURL = "http://localhost:3000";
+  const baseURL = "http://cp-match-backendp4.herokuapp.com";
 
   const emptyPost = {
     title: "",
